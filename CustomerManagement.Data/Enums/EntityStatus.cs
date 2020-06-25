2adb591724084874e7e48bebdf68548e9e2b13b7
pntien <42037314+pntien@users.noreply.github.com>
2020-06-25 14:16:12 +0700

    Add Brand Service

---
+++ CustomerManagement.Data/Enums/EntityStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerManagement.Data.Enums
+{
+    public enum EntityStatus
+    {
+        ACTIVE = 1,
+        LOCK,
+        HIDDEN,
+        DELETED = 99
+    }
+}
